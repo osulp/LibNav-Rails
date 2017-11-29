f27b962857036a634e86ed67f1ab32e232dc5e65
Brandon Straley <straleyb@onid.oregonstate.edu>
2017-11-29 09:40:56 -0800

    Navbar toggling and retention of state for navbar

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'floors#index'
   resources :floors
-  get "/floor/:floor_number", to: "floors#chosen_floor", as: "chosen_floor"
+  get "/floor/:floor_number(/:toggle_navbar)", to: "floors#chosen_floor", as: "chosen_floor"
   post "/search", to: "home#search", as: "search"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
