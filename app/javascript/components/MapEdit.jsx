ec4da50cf940ab9f46be3fe19f000f050a7d17c3
Fischer Jemison <jemisonf@oregonstate.edu>
2017-12-07 14:12:17 -0800

    Integrates grid editing with map and buttons interface

--- app/javascript/components/MapEdit.jsx
+++ app/javascript/components/MapEdit.jsx
@@ -7,12 +7,9 @@ require('d3');
 
 class EditMap extends React.Component {
   static propTypes = {
-    // bounding_box_height: PropTypes.number,
-    // bounding_box_width: PropTypes.number,
-    // bounding_box_x: PropTypes.number,
-    // bounding_box_y: PropTypes.number,
-    // gridSize: PropTypes.number,
+    mapUrl: PropTypes.string,
     id: PropTypes.string,
+    current_selected_floor: PropTypes.string,
     locations: PropTypes.array
   }
   static defaultProps = {
@@ -27,8 +24,8 @@ class EditMap extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      locations: this.props.locations,
-      locationsBoxes: this.props.locations.map((location => <LocationBox key={location.id} position_x={location.position_x} position_y={location.position_y} width={location.width} height={location.height} id={location.id} />))
+      locations: this.props.locations.filter(location => location.floor_id == this.props.id),
+      locationsBoxes: this.props.locations.map((location => location.floor_id == this.props.id ? (<LocationBox key={location.id} position_x={location.position_x} position_y={location.position_y} width={location.width} height={location.height} id={location.id} />) : null))
     }
     console.log(this.state.locationsBoxes);
   }
@@ -38,16 +35,18 @@ class EditMap extends React.Component {
     });
   }
 
+  componentWillReceiveProps = (nextProps) => {
+    this.render_svg(nextProps.mapUrl);
+    this.setState({
+      locations: nextProps.locations.filter(location => location.floor_id == nextProps.id),
+      locationsBoxes: nextProps.locations.map((location => location.floor_id == nextProps.id ? (<LocationBox key={location.id} position_x={location.position_x} position_y={location.position_y} width={location.width} height={location.height} id={location.id} />) : null))
+    })
+  }
   componentDidMount = () => {
-    document.getElementById(`${this.props.id}-slider`).value = parseInt(this.state.gridSize);
-    document.getElementById(`${this.props.id}-slider`)
-      .addEventListener('input', this.updateGridSize);
-    $(`#${this.props.id}-save-btn`).on('click', (event) => {
-      this.saveFloor(event);
-    });
+    this.render_svg(this.props.mapUrl);
+    $(`#floor-${this.props.current_selected_floor}-save-btn`).on('click', this.saveFloor);
   }
 
-  // the following two functions use d3 helpers & are more concise/readable than just using jquery
   draggedCircle = d => {
     let diffX = d3.event.dx;
     let diffY = d3.event.dy;
@@ -59,13 +58,6 @@ class EditMap extends React.Component {
   }
 
   draggedBox(d) {
-    // console.log('dragging');
-    // let diffX = d3.event.dx;
-    // let diffY = d3.event.dy;
-    // this.setState({
-    //   bounding_box_y: Math.max(Math.min(this.state.bounding_box_y + diffY, 800 - this.state.bounding_box_height), 0),
-    //   bounding_box_x: Math.max(Math.min(this.state.bounding_box_x + diffX, 800 - this.state.bounding_box_width), 0)
-    // });
     d3.select(this)
       .attr("x", d.x = d3.event.x)
       .attr("y", d.y = d3.event.y);
@@ -73,7 +65,7 @@ class EditMap extends React.Component {
 
   saveFloor = (event) => {
     console.log("clicked");
-    let floorId = this.props.id.split('-').pop();
+    let floorId = this.props.id;
     let token = $('meta[name="csrf-token"]').attr('content');
     let locations_attributes = []
     Object.keys(this.state.locations).forEach((key) => {
@@ -87,25 +79,11 @@ class EditMap extends React.Component {
       new_attributes.position_y = parseInt(group.find('rect').attr('y'));
       locations_attributes.push(new_attributes);
     })
-    // let locations_attributes = {}
-    // Object.keys(this.state.locations).forEach((key, index) => {
-    //   let id = this.state.locations[key].id
-    //   let group = $(`#location-box-${id}`)
-    //   locations_attributes[index] = {};
-    //   locations_attributes[index].floor_id = floorId;
-    //   locations_attributes[index].id = id;
-    //   locations_attributes[index].height = parseInt(group.find('rect').attr('height'));
-    //   locations_attributes[index].width = parseInt(group.find('rect').attr('width'));
-    //   locations_attributes[index].position_x = parseInt(group.find('rect').attr('x'));
-    //   locations_attributes[index].position_y = parseInt(group.find('rect').attr('y'));
-    //   // locations_attributes[index]._destroy = false;
-    // })
     console.log(locations_attributes);
     let foo = null;
     $.ajax({
       url: `floors/${floorId}.json`,
       type: 'patch',
-      // contentType: 'application/json',
       beforeSend: (xhr) => {
         xhr.setRequestHeader('X-CSRF-Token', token);
       },
@@ -119,23 +97,29 @@ class EditMap extends React.Component {
     })
   }
 
+  render_svg = (mapUrl) => {
+    var svgContainer = d3.select(".svgContainer");
+    if (d3.select('.map_image')) {
+      d3.select('.map_image').remove();
+    }
+
+    svgContainer.append('svg:image')
+      .attr("class", "map_image")
+      .attr("xlink:href", mapUrl)
+      .attr("x", 0)
+      .attr("y", 0)
+      .attr("width", 800)
+      .attr("height", 800)
+      .lower();
+  }
+
   render() {
     return (
       <div className='map' >
         <div className="row">
           <div className="col">
-            <svg height="800px" width="800px" id={`${this.props.id}-svg`}>
-              {/* <pattern id="grid" width={this.state.grid_size + "px"} height={this.state.grid_size + "px"} patternUnits="userSpaceOnUse">
-                <path d="M 100 0 L 0 0 0 100" fill="none" stroke="gray" strokeWidth="1" />
-              </pattern>
-              <rect id={`${this.props.id}-bounding-box`} className="bounding-box" x={this.state.bounding_box_x + "px"} y={this.state.bounding_box_y + "px"} width={this.state.bounding_box_width + "px"} height={this.state.bounding_box_height + "px"} fill="url(#grid)" stroke="black" />
-              <circle id={`${this.props.id}-drag-circle`} className="drag-circle" r="10px" cx={this.state.bounding_box_x + this.state.bounding_box_width + "px"} cy={this.state.bounding_box_y + this.state.bounding_box_height + "px"} >
-              <text>BLAH</text>
-              </circle> */}
+            <svg height="800px" width="800px" className="svgContainer" id={`floor-${this.props.current_selected_floor}-svg`}>
               {
-                // this.props.locations.map((location) => {
-                //   <LocationBox position_x={location.position_x} position_y={location.position_y} width={location.width} height={location.height} id={location.id} />
-                // })
                 this.state.locationsBoxes
               }
             </svg>
@@ -143,17 +127,7 @@ class EditMap extends React.Component {
         </div>
         <div className="row">
           <div className="col">
-            <form className="form-inline">
-              <div className="form-group">
-                <label htmlFor={`${this.props.id}-slider`}>Grid Size:</label>
-                <input type="range" min="5" max="50" id={`${this.props.id}-slider`} />
-              </div>
-            </form>
-          </div>
-          <div className="col">
-            <button className="btn btn-primary" id={`${this.props.id}-save-btn`}>
-              Save
-            </button>
+            <button id={`floor-${this.props.current_selected_floor}-save-btn`} className="btn btn-primary">Save</button>
           </div>
         </div>
       </div >);
