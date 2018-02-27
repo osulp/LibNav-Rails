945085185ea3e0713d135563997b05e4ffc47837
Brandon Straley <straleyb@onid.oregonstate.edu>
2018-02-27 09:53:48 -0800

    add back in legend

---
+++ app/javascript/components/Legend.jsx
@@ -0,0 +1,42 @@
+import React from "react"
+import PropTypes from "prop-types"
+import LegendListElement from './LegendListElement'
+
+class Legend extends React.Component {
+
+  render() {
+    return (
+        <div className="row">
+          <div className="col-4"></div>
+          <div className="col-4">
+            <div>
+              <div className="card">
+                <div className="card-header" id="headingOne">
+                  <h5 className="mb-0">
+                    Map Legend
+                  </h5>
+                </div>
+                <div>
+                  <div className="card-block">
+                    <ul className="list-unstyled">
+                    {this.props.icon_set.map((icon, i) => {
+                      return(
+                        <LegendListElement key={"searched_" + i} icon_url={icon.icon_url} icon_name={icon.icon_name} />
+                      );
+                    })}
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-4"></div>
+        </div>
+    );
+  }
+}
+Legend.propTypes = {
+  icon_set: PropTypes.array,
+  current_selected_floor: PropTypes.string
+};
+export default Legend
