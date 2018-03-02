d294d17f610aa930972097b69b6917a4ccf7e93c
Brandon R. Straley <straleyb@onid.oregonstate.edu>
2018-03-02 09:02:51 -0800

    Makes tag unique (#270)

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,7 +2,7 @@ class Tag < ApplicationRecord
   scope :has_location, -> { where.not(location_id: nil) }
   belongs_to :location, optional: true
   scoped_search on: [:label]
-  validates :label, presence: true
+  validates :label, presence: true, uniqueness: true
 
   def name
     label
