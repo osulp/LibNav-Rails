fb07b7734b398548e2bfc13dff1062a888b24cd4
Josh Gum <revgum@gmail.com>
2018-04-05 14:26:22 -0700

    fixes background highlighting bug

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180404151516) do
+ActiveRecord::Schema.define(version: 20180405211415) do
 
   create_table "floors", force: :cascade do |t|
     t.string "name"
