9ed47f06e2db881cf5d88c0c8dc7506020fc801a
Ryan Ordway <1882223+decimalator@users.noreply.github.com>
2019-10-02 16:49:42 -0700

    Remove datadog

--- config/initializers/datadog-tracer.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-if %w[production staging].include? Rails.env
-  Datadog.configure do |c|
-    c.use :rails, service_name: "lib-nav-#{Rails.env}"
-    c.use :http, service_name: "lib-nav-#{Rails.env}-http"
-  end
-end
